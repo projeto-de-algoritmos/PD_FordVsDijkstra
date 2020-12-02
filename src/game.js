cc23fc2a2256e86e44df1ba953aa09ae8db666fe
Sam Buters <sambuters@gmail.com>
2020-12-01 23:17:31 -0300

    Adding interactive graph interface drawing
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>

--- src/game.js
+++ src/game.js
@@ -1,15 +1,58 @@
-var EmptyScene = new Phaser.Class({
+var MainScene = new Phaser.Class({
 	Extends: Phaser.Scene,
-	initialize: function EmptyScene() {
-		Phaser.Scene.call(this, {key: 'empty_scene'});
+	initialize: function MainScene() {
+		Phaser.Scene.call(this, {key: 'main_scene'});
 	},
 
 	preload: function () {
-		console.log('Nothing.')
+		this.load.image('circle', 'assets/circle.png');
 	},
 
 	create: function () {
-		console.log('Nothing.')
+		let tempGraphics = this.add.graphics({lineStyle: {width: 2, color: 0xffffff}});
+		let permGraphics = this.add.graphics({lineStyle: {width: 2, color: 0xffffff}});
+		let line;
+		
+		
+		let isCreating = false;
+		let circle = this.add.image(0, 0, 'circle').setOrigin(0.5);
+		circle.setScale(0.05);
+		
+		this.input.on('pointermove', (e) => {
+			if(isCreating) {
+				line.x2 = e.position.x;
+				line.y2 = e.position.y;
+				
+				tempGraphics.clear();
+				tempGraphics.strokeLineShape(line);
+			}
+			
+			else {
+				circle.x = e.position.x;
+				circle.y = e.position.y;
+			}
+		});
+		
+		this.input.on('pointerup', (e) => {
+			if(isCreating) {
+				isCreating = false;
+				circle.setAlpha(1);
+				this.add.image(e.position.x, e.position.y, 'circle').setOrigin(0.5).setScale(0.05);
+				
+				permGraphics.strokeLineShape(line);
+				tempGraphics.clear();
+			}
+			
+			else {
+				isCreating = true;
+				circle.setAlpha(0);
+				
+				this.add.image(e.position.x, e.position.y, 'circle').setOrigin(0.5).setScale(0.05);
+				
+				line = new Phaser.Geom.Line(e.position.x, e.position.y, e.position.x, e.position.y);
+				tempGraphics.strokeLineShape(line);
+			}
+		});
 	}
 });
 
@@ -17,7 +60,14 @@ var config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [EmptyScene]
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: 0,
+			debug: false
+		}
+	},
+    scene: [MainScene]
 };
 
 var game = new Phaser.Game(config);
