ef9735d17102fe5d3b3651a029f42e7677649d6c
Sam Buters <sambuters@gmail.com>
2020-12-03 23:18:24 -0300

    Adding dijkstra algorithm
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>

--- src/utils.js
+++ src/utils.js
@@ -90,3 +90,40 @@ function setElectrical() {
        multiplier = 1;
     }
 }
+
+function dijkstra(graph, start_node, last_node) {
+	let priority_queue = new Heapify(graph.getSize());
+	let distance = {};
+	let previous = {};
+
+	distance[start_node] = 0;
+	priority_queue.push({index: start_node, node: graph.getVertex(start_node)}, 1);
+
+	graph.adjList.forEach((node, index) => {
+		if(index != start_node)
+			distance[index] = Infinity;
+		previous[index] = null;
+	});
+
+	while(priority_queue.size) {
+		let current_node = priority_queue.pop();
+
+		current_node.node.edges.forEach((edge) => {
+			let cost = edge.weight + distance[current_node.index];
+
+			if(cost < distance[edge.edge]) {
+				distance[edge.edge] = cost;
+				previous[edge.edge] = current_node.index;
+
+				priority_queue.push({index: edge.edge, node: graph.getVertex(edge.edge)}, cost);
+			}
+		});
+	}
+
+	let best_path = [last_node];
+
+	while(best_path[0] != start_node)
+		best_path.unshift(previous[best_path[0]]);
+
+	return best_path;
+}
